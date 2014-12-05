79af6578870ca8615b7f31439eab6c418f4a3fbc
smis394 <shambhavimishra46@gmail.com>
2014-12-04 19:01:57 -0700

    Update Point.java

--- src/Point.java
+++ src/Point.java
@@ -1,1 +1,54 @@
 // A Point object represents a pair of (x, y) coordinates.
+import java.lang.Math;
+public class Point
+{
+  private int x;
+  private int y;
+  
+  public Point()
+  {
+    x=0;
+    y=0;
+  }
+  public Point(int initx, int inity)
+ {
+    x=initx;
+    y=inity;
+  }
+  
+  public int getX()
+  {
+    return x;
+  }
+  public int getY()
+  {
+    return y;
+  }
+  public double distanceFromOrigin()
+  {
+    double d=Math.sqrt(x*x+y*y);
+    return d;
+  }
+  public double distance (Point p)
+  {
+    double d=Math.sqrt(((x-p.getX())*(x-p.getX()))+((y-p.getY())*(y-p.getY())));
+    return d;
+  }
+  public String toString()
+  {
+    return "("+x+","+y+")";
+  }
+  public void setX(int x1)
+  {
+    x=x1;
+  }
+  public void setY(int y2)
+  {
+    y=y2;
+  }
+  public void translate (int x1, int y1)
+  {
+    x+=x1;
+    y+=y1;
+  }
+}
